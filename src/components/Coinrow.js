ceaf11e01ae49f7f280562dcbbc151ee38f1bc68
Jeong-wonho <kallzzang@naver.com>
2022-07-14 23:49:37 +0900

    배포 연습

--- src/components/Coinrow.js
+++ src/components/Coinrow.js
@@ -26,12 +26,12 @@ export const Coinrow = ({
         selectCoin();
       }}
     >
-      <td>{name}</td>
-      <td>{Math.round(coin.trade_price.toFixed(1))} KRW</td>
-      <td className="text-red-500">{coin.high_price} KRW</td>
-      <td className="text-blue-500">{coin.low_price} KRW</td>
-      <td>{coin.acc_trade_volume_24h.toFixed(2)}</td>
-      <td className={coin.change === "FALL" ? "text-blue-500" : "text-red-500"}>
+      <td className={"pt-4"}>{name}</td>
+      <td className={"pt-4"}>{Math.round(coin.trade_price.toFixed(1))} KRW</td>
+      <td className="text-red-500 pt-4">{coin.high_price} KRW</td>
+      <td className="text-blue-500 pt-4">{coin.low_price} KRW</td>
+      <td className={"pt-4"}>{coin.acc_trade_volume_24h.toFixed(2)}</td>
+      <td className={`pt-4 ${coin.change === "FALL" ? "text-blue-500" : "text-red-500"}`}>
         {changeLiteral(coin.change)}
         {(coin.change_rate * 100).toFixed(2)}%
       </td>
